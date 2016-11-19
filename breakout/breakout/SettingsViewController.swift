e616e787815b34d81009a0ff68da456629407b28
Luke Holoubek <lukasholoubek@3DR-Luke.local>
2016-11-19 12:14:03 -0800

    Work on settings VC

--- breakout/breakout/SettingsViewController.swift
+++ breakout/breakout/SettingsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +22,21 @@ class SettingsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
+
+
+//
+//extension SettingsViewController: UITableViewDelegate {
+//    
+//    
+//    
+//    
+//}
+//
+//
+//extension SettingsViewController: UITableViewDataSource {
+//    
+//    
+//    
+//}
